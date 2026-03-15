218ffa989f3f86b4796f30ab6ae48d253d95c9d7
Huong <thuhuongtk21@gmail.com>
2026-03-15 19:01:03 +0700

    First commit

---
+++ src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class MyController {
+    @GetMapping("/")
+    public String index() {
+        return "index"; // Nó sẽ tự tìm file index.html ở bước trên
+    }
+
+    @GetMapping("/massage-tri-lieu")
+    public String massageTriLieuPage() {
+        return "massage-tri-lieu"; // Tên file HTML vừa tạo (không cần .html)
+    }
+
+    @GetMapping("/ve-chung-toi")
+    public String veChungToiPage() {
+        return "ve-chung-toi"; // Trả về file ve-chung-toi.html
+    }
+
+    @GetMapping("/bai-massage-ten-tuoi")
+    public String baiMassageTenTuoiPage() {
+        return "bai-massage-ten-tuoi"; // Trả về file bai-massage-ten-tuoi.html
+    }
+
+    @GetMapping("/video-khach-hang")
+    public String videoKhachHangPage() {
+        return "video-khach-hang"; // Trả về file video-khach-hang.html
+    }
+}
